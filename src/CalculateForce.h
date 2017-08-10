9ee9ebb2ea51b4adf7d794c48cf549acf6a2ce7d
Denis V. Esipov <esipov@ict.sbras.ru>
2017-08-10 20:24:36 +0700

    Upload New File

---
+++ src/CalculateForce.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "stdafx.h"
+#include "Grid.h"
+#include "SolidBody.h"
+
+using namespace std;
+
+double DeltaFunction(double x, double y, Grid grid);
+double FunctionD(double r);
+void GetInfluenceArea(int& i_min, int& i_max, int& j_min, int& j_max, double x, double y, int size, Grid grid);
+
+double CalculateForce_X(Matrix& force_x, list<Circle> &iList, Matrix& u, double r, double & Coeff, Grid grid, double alpha_f, double beta_f, double M);
+double CalculateForce_Y(Matrix& force_y, list<Circle> &iList, Matrix& v, double r, double & Coeff, Grid grid, double alpha_f, double beta_f, double M);
