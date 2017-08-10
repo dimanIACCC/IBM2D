773f5bd61c055cdd9160176ad7d3c37e86332497
Denis V. Esipov <esipov@ict.sbras.ru>
2017-08-10 20:27:28 +0700

    Upload New File

---
+++ src/stdafx.h
@@ -0,0 +1,20 @@
+// stdafx.h: включаемый файл дл€ стандартных системных включаемых файлов
+// или включаемых файлов дл€ конкретного проекта, которые часто используютс€, но
+// не часто измен€ютс€
+//
+
+#pragma once
+#define _USE_MATH_DEFINES
+
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+//#include <map>
+#include <list>
+#include <string>
+#include <chrono>
+
+
+// TODO: ”становите здесь ссылки на дополнительные заголовки, требующиес€ дл€ программы
