428aad48fd9273658675cdef8901ab31fde0ca41
dimaniac <dimaniaccc@gmail.com>
2017-08-25 14:56:14 +0700

    Output: replace Geometry by 1D Zone for Circles         add second component of velocity to u and v files

--- src/Output.cpp
+++ src/Output.cpp
@@ -13,8 +13,9 @@ void OutputPressure(Matrix data, int n, double output_step, list<Circle> iList, 
 	output.open(filename.c_str());
 
 	output << "title = " << '"' << "sample mesh" << '"' << endl;
-	output << "Variables=" << '"' << 'x' << '"' << ',' << '"' << 'y' << '"' << ',' << '"' << 'p' << '"' << endl;
+	output << "Variables = x y p" << endl;
 	output << "zone T=" << '"' << n << '"' << ",  i=" << grid.N1 + 1 << ", j=" << grid.N2 + 1 << ", f=point" << endl;
+	output << "SolutionTime = " << n << endl;
 
 
 	output << 0.0 << ' ' << 0.0 << ' ' << data[0][0] << endl;
@@ -49,18 +50,13 @@ void OutputPressure(Matrix data, int n, double output_step, list<Circle> iList, 
 	output << (i - 1)*grid.d_x << ' ' << (j - 1)*grid.d_y << ' ' << data[i][j] << endl;
 
 	for (auto& solid : iList){
-
-		output << "GEOMETRY ZN = " << n / output_step + 1 << ", X=" << solid.Nodes[0].x[1] << ", Y=" << solid.Nodes[0].x[2] << ", T=LINE, LT = 0.3, CS=GRID" << endl;
-		output << "1" << endl << grid.NF + 1 << endl;
-		output << 0 << " " << 0 << endl;
-		for (int i = 1; i < grid.NF; ++i){
-
-			output << solid.Nodes[i].x[1] - solid.Nodes[0].x[1] << " " 
-			       << solid.Nodes[i].x[2] - solid.Nodes[0].x[2] << endl;
-
+		output << "zone T = circle" << ",  i=" << solid.Nn << ", f=point" << endl;
+		output << "SolutionTime = " << n << endl;
+		for (int i = 0; i < solid.Nn; ++i){
+			output << solid.Nodes[i].x[1] << " "
+			       << solid.Nodes[i].x[2] << " "
+			       << 0 << endl;
 		}
-		output << 0 << " " << 0 << endl;
-		output << endl;
 	}
 
 
@@ -81,39 +77,35 @@ void OutputVelocity_U(Matrix data, int n, int output_step, list<Circle> iList, G
 	output.open(filename.c_str());
 
 	output << "title = " << '"' << "sample mesh" << '"' << endl;
-	output << "Variables=" << '"' << 'x' << '"' << ',' << '"' << 'y' << '"' << ',' << '"' << 'u' << '"' << endl;
+	output << "Variables = x y u v" << endl;
 	output << "zone T=" << '"' << n << '"' << ",  i=" << grid.N1 << ", j=" << grid.N2 + 1 << ", f=point" << endl;
 
 	for (int i = 0; i < grid.N1; ++i){
 
-		output << i*grid.d_x << ' ' << 0.0 << ' ' << data[i][0] << endl;
+		output << i*grid.d_x << ' ' << 0.0 << ' ' << data[i][0] << ' ' << 0 << endl;
 	}
 
 	for (int j = 1; j < grid.N2; ++j){
 		for (int i = 0; i < grid.N1; ++i){
-			output << (i)*grid.d_x << ' ' << (j - 0.5)*grid.d_y << ' ' << data[i][j] << endl;
+			output << (i)*grid.d_x << ' ' << (j - 0.5)*grid.d_y << ' ' << data[i][j] << ' ' << 0 << endl;
 		}
 	}
 
 	for (int i = 0; i < grid.N1; ++i){
 
 		int j = grid.N2;
-		output << i*grid.d_x << ' ' << (j - 1)*grid.d_y << ' ' << data[i][j] << endl;
+		output << i*grid.d_x << ' ' << (j - 1)*grid.d_y << ' ' << data[i][j] << ' ' << 0 << endl;
 	}
 
 	for (auto& solid : iList){
-
-		output << "GEOMETRY ZN = " << n / output_step + 1 << ", X=" << solid.Nodes[0].x[1] << ", Y=" << solid.Nodes[0].x[2] << ", T=LINE, LT = 0.3, CS=GRID" << endl;
-		output << "1" << endl << grid.NF + 1 << endl;
-		output << 0 << " " << 0 << endl;
-		for (int i = 1; i < grid.NF; ++i){
-
-			output << solid.Nodes[i].x[1] - solid.Nodes[0].x[1] << " "
-			       << solid.Nodes[i].x[2] - solid.Nodes[0].x[2] << endl;
-
+		output << "zone T = circle" << ",  i=" << solid.Nn << ", f=point" << endl;
+		output << "SolutionTime = " << n << endl;
+		for (int i = 0; i < solid.Nn; ++i) {
+			output << solid.Nodes[i].x[1] << " "
+			       << solid.Nodes[i].x[2] << " "
+			       << solid.Nodes[i].uf[1] << " "
+			       << solid.Nodes[i].uf[2] << " " << endl;
 		}
-		output << 0 << " " << 0 << endl;
-		output << endl;
 	}
 
 	output.close();
@@ -133,39 +125,35 @@ void OutputVelocity_V(Matrix& data, int n, int output_step, list<Circle> iList, 
 	output.open(filename.c_str());
 
 	output << "title = " << '"' << "sample mesh" << '"' << endl;
-	output << "Variables=" << '"' << 'x' << '"' << ',' << '"' << 'y' << '"' << ',' << '"' << 'v' << '"' << endl;
+	output << "Variables = x y u v" << endl;
 	output << "zone T=" << '"' << n << '"' << ",  i=" << grid.N1 + 1 << ", j=" << grid.N2 << ", f=point" << endl;
 
 
 	for (int j = 0; j < grid.N2; ++j){
 
 
-		output << 0.0 << ' ' << (j)*grid.d_y << ' ' << data[0][j] << endl;
+		output << 0.0 << ' ' << (j)*grid.d_y << ' ' << 0 << ' ' << data[0][j] << endl;
 
 		for (int i = 1; i < grid.N1; ++i){
 
-			output << (i - 0.5)*grid.d_x << ' ' << (j)*grid.d_y << ' ' << data[i][j] << endl;
+			output << (i - 0.5)*grid.d_x << ' ' << (j)*grid.d_y << ' ' << 0 << ' ' << data[i][j] << endl;
 
 		}
 
 		int i = grid.N1;
 
-		output << (i - 1)*grid.d_x << ' ' << (j)*grid.d_y << ' ' << data[i][j] << endl;
+		output << (i - 1)*grid.d_x << ' ' << (j)*grid.d_y << ' ' << 0 << ' ' << data[i][j] << endl;
 	}
 
 	for (auto& solid : iList){
-
-		output << "GEOMETRY ZN = " << n / output_step + 1 << ", X=" << solid.Nodes[0].x[1] << ", Y=" << solid.Nodes[0].x[2] << ", T=LINE, LT = 0.3, CS=GRID" << endl;
-		output << "1" << endl << grid.NF + 1 << endl;
-		output << 0 << " " << 0 << endl;
-		for (int i = 1; i < grid.NF; ++i){
-
-			output << solid.Nodes[i].x[1] - solid.Nodes[0].x[1] << " " 
-			       << solid.Nodes[i].x[2] - solid.Nodes[0].x[2] << endl;
-
+		output << "zone T = circle" << ",  i=" << solid.Nn << ", f=point" << endl;
+		output << "SolutionTime = " << n << endl;
+		for (int i = 0; i < solid.Nn; ++i) {
+			output << solid.Nodes[i].x[1] << " "
+			       << solid.Nodes[i].x[2] << " "
+			       << solid.Nodes[i].uf[1] << " "
+			       << solid.Nodes[i].uf[2] << " " << endl;
 		}
-		output << 0 << " " << 0 << endl;
-		output << endl;
 	}
 
 	output.close();
